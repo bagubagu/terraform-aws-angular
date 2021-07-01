a5380e9204fb3e0888d5c5c1fe0bbe97b11b3da2
Trent Stollery <trent.stollery@millowgroup.com>
2021-07-01 15:27:24 +0930

    updates to fix locals

--- variables.tf
+++ variables.tf
@@ -5,3 +5,10 @@ variable "hosted_zone" {}
 variable "force_destroy" {
   default = false
 }
+
+variable "region" {
+  default = "us-east-1"
+}
+
+  region           = "${var.region}"
+  s3_origin_id     = "${var.bucket}"
