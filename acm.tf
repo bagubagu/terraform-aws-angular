43f32ac3c056c4d788f703f662766267cd58d7a9
Trent Stollery <trent.stollery@millowgroup.com>
2021-07-01 17:05:22 +0930

    updates to fix for issue

--- acm.tf
+++ acm.tf
@@ -38,7 +38,7 @@ resource "aws_route53_record" "cert_validation" {
 
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = "${aws_acm_certificate.cert.arn}"
-  validation_record_fqdns = ["${aws_route53_record.cert_validation.fqdn}"]
+  validation_record_fqdns = ["${aws_route53_record.cert_validation[each.key].fqdn}"]
 }
 
 output "cert_arn" {
